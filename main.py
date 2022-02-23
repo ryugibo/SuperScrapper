53d6dbb32c2b929028a42e1dc919197ad2b219b5
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-23 11:35:11 +0000

    :memo: #4.4 | Scrapper Integration part two
    - 스택오버플로우 스크랩퍼 연동

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-
+from scrapper import get_jobs
 
 app = Flask("Super Scrapper")
 
@@ -12,6 +12,8 @@ def report():
   word = request.args.get("word")
   if word:
     word = word.lower()
+    jobs = get_jobs(word)
+    print(jobs)
   else:
     return redirect("/")
   return render_template("report.html", searchingBy = word)
