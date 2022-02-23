234c13ceb20bfbd3c410acf28e8b5de03e88aef8
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-23 10:48:05 +0000

    :memo: #4.3 | Forms and Query Arguments

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask("Super Scrapper")
 
 @app.route("/")
 def home():
   return render_template("potato.html")
+
+@app.route("/report")
+def report():
+  word = request.args.get("word")
+  return render_template("report.html", searchingBy = word)
 
 app.run(host = "0.0.0.0")
