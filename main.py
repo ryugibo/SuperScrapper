4a8d084470249a1c4d3d060f4f81d5162447d240
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-23 10:36:53 +0000

    :memo: #4.2 | Dynamic URLs and Templates

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("Super Scrapper")
 
 @app.route("/")
 def home():
-  return "Hello! Welcome to mi casa!"
-
-@app.route("/contact")
-def potato():
-  return "Contact me!"
+  return render_template("potato.html")
 
 app.run(host = "0.0.0.0")
