955dbe91d5845c0c231f82172fa55ca89c0aade0
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-24 13:22:24 +0000

    :memo: #4.8 | File Download

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from scrapper import get_jobs
+from exporter import save_to_file
 
 app = Flask("Super Scrapper")
 
@@ -37,7 +38,8 @@ def export():
     jobs = db.get(word)
     if not jobs:
       raise Exception()
-    return f"Generate CSV for { word }"
+    save_to_file(jobs)
+    return send_file("jobs.csv")
   except:
     return redirect("/")
   
